ba5b477a3ccab8d326bf44ad49fcb1dd67c53b49
Degan90 <poorya.degan@gmail.com>
2021-08-12 22:56:23 -0400

    mobile responsive

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,18 +13,18 @@ const Contact = () => {
                 
                 <div>
                     <img className="icon" src={email} />
-                    <a href="mailto: poorya.degan@gmail.com">poorya.degan@gmail.com</a>
+                    <a href="mailto: poorya.degan@gmail.com" className="ap">poorya.degan@gmail.com</a>
                 </div>
                
                 
                 <div>
                     <img className="icon" src={linkedin} />
-                    <a href="https://www.linkedin.com/in/degan90">www.linkedin.com/in/degan90</a>
+                    <a href="https://www.linkedin.com/in/degan90" className="ap">www.linkedin.com/in/degan90</a>
                 </div>
 
                 <div> 
                   <img className="icon" src={github} />  
-                  <a href="https://github.com/Degan90">https://github.com/Degan90</a>
+                  <a href="https://github.com/Degan90" className="ap">https://github.com/Degan90</a>
                  </div>
 
 
