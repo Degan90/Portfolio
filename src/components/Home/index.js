199ad1ac745d287d42f6ce53fedd6fc55403da3f
Degan90 <poorya.degan@gmail.com>
2021-08-13 17:17:36 -0400

    added github to project

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,29 +1,30 @@
 import { Link } from "react-router-dom";
 
-import linkedin from "../img/linkedin.png"
-import email from "../img/email.png"
-import phone from "../img/phone.png"
-import github from "../img/github.png"
+import linkedin from "../img/linkedin.png";
+import email from "../img/email.png";
+import phone from "../img/phone.png";
+import github from "../img/github.png";
 import About from "../About";
 import Project from "../Project";
 import Contact from "../Contact";
-import pourya from "../img/pourya.jpg"
-import pourya1 from "../img/pourya1.jpg"
-import orange from "../img/orange.jpg"
-import Image from 'react-bootstrap/Image'
-import Col from 'react-bootstrap/Col';
+import pourya from "../img/pourya.jpg";
+import pourya1 from "../img/pourya1.jpg";
+import orange from "../img/orange.jpg";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap";
 const Home = () => {
-    return (<div>
-        <div className="home">
-            <div className="title">
-                <div>
-                    <h3 className="myName">Hi , my name is</h3>
-                    <h1 className="name">Pourya Dehghan</h1>
-                    <h1 className="job">I build things for the web.</h1>
-                </div>
-                <div className="information">
-
-                    {/* <Link to="/contact" >
+  return (
+    <div>
+      <div className="home">
+        <div className="title">
+          <div>
+            <h3 className="myName">Hi , my name is</h3>
+            <h1 className="name">Pourya Dehghan</h1>
+            <h1 className="job">I build things for the web.</h1>
+          </div>
+          <div className="information">
+            {/* <Link to="/contact" >
                         <h4>Contact</h4>
                     </Link>
                     <Link to="/project" >
@@ -32,43 +33,44 @@ const Home = () => {
                     <Link to="/about">
                         <h4>About</h4>
                     </Link> */}
-                    <div>
-                        
-                        <a href="mailto: poorya.degan@gmail.com"><img className="icon" src={email} /></a>
-                    </div>
-
-
-                    <div>
-                        
-                        <a href="https://www.linkedin.com/in/degan90"><img className="icon" src={linkedin} /></a>
-                    </div>
-
-                    <div className="phone">
-                        
-                        <a href="tel:4043845765"><img className="icon" src={phone} /></a>
-                    </div>
-                    <div>
-                        
-                        <a href="https://github.com/Degan90"><img className="icon" src={github} /></a>
-                    </div>
-
-                </div>
-
+            <div>
+              <a href="mailto: poorya.degan@gmail.com">
+                <img className="icon" src={email} />
+              </a>
             </div>
 
-            {/* <div>
+            <div>
+              <a href="https://www.linkedin.com/in/degan90">
+                <img className="icon" src={linkedin} />
+              </a>
+            </div>
+
+            {/* <div className="phone">
+                        
+                        <a href="tel:4043845765"><img className="icon" src={phone} /></a>
+                    </div> */}
+            <div>
+              <a href="https://github.com/Degan90">
+                <img className="icon" src={github} />
+              </a>
+            </div>
+            <div className="btnHome">
+              <Button variant="outline-success" href="/resume">
+                R
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* <div>
             <Col xs={6} md={4}>
                 <Image className="firstImage" src={pourya} rounded />
             </Col>
                 <img className="firstImage" src={pourya} />
             </div> */}
-        </div>
-            
-        
-        </div>
-
-
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,6 +3,7 @@ import heroes from "../img/heroes.jpg";
 import dotaSimon from "../img/dotaSimon.png";
 import Carousel from "react-bootstrap/Carousel";
 import { useState } from "react";
+import greengh from "../img/greengh.png";
 const Project = () => {
   const [index, setIndex] = useState(0);
 
@@ -11,84 +12,91 @@ const Project = () => {
   };
 
   return (
-      <>
-      
-     <h1 className="projectTitle" >The Apps I've built</h1>
-            
-    <section className="allProject">
-     
-      <div className="project">
-        <div className="projectDes">
-          <h2>Dota2_Simon</h2>
+    <>
+      <h1 className="projectTitle">The Apps I've built</h1>
 
-          <h5>
-            This is a Simon game which players must repeat random sequences of
-            button by clicking the buttons in corretct order. The diffrences
-            between this game and the other simon games are i used the Dota 2
-            characters's images and sounds to make it more interesting.
-          </h5>
-        </div>
-
-        <div>
-          <a href="https://hungry-meninsky-f0a920.netlify.app/">
-            <img className="projectImage" src={dotaSimon} />
-          </a>
-        </div>
-      </div>
-      <div className="project">
-        <div className="projectDes">
-          <h2>Dota2 Heroes</h2>
-
-          <h5>
-            My App is called Dota2 Heroes It's an app that shows the all the
-            Dota 2 heroes and if you click on hero gives you the detail and
-            attributes of that hero.
-          </h5>
-        </div>
-
-        <div>
-        <a href="https://pedantic-edison-4080c1.netlify.app">
-            <img className="projectImage" src={heroes} />
-          </a>
-        </div>
-      </div>
-
-
-      <div className="project">
-        <div>
-          <h2>Project 3</h2>
+      <section className="allProject">
+        <div className="project">
           <div className="projectDes">
+            <h2>Dota2_Simon</h2>
+
             <h5>
-              he standard chunk of Lorem Ipsum used since the 1500s is
-              reproduced below for those interested. Sections 1.10.32 and
-              1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-              reproduced in their exact original form, accompanied by English
-              versions from the 1914 translation by H. Rackham.
+              This is a Simon game which players must repeat random sequences of
+              button by clicking the buttons in corretct order. The diffrences
+              between this game and the other simon games are i used the Dota 2
+              characters's images and sounds to make it more interesting.
             </h5>
+            <div>
+              <a href="https://github.com/Degan90/Simon-Dota.git">
+                <img className="icon" src={greengh} />
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <a href="https://hungry-meninsky-f0a920.netlify.app/">
+              <img className="projectImage" src={dotaSimon} />
+            </a>
           </div>
         </div>
-        <div>
-          <img className="projectImage" src={background} />
-        </div>
-      </div>
-      <div className="project">
-        <div>
-          <h2>Project 4</h2>
+        <div className="project">
           <div className="projectDes">
+            <h2>Dota2 Heroes</h2>
+
             <h5>
-              he standard chunk of Lorem Ipsum used since the 1500s is
-              reproduced below for those interested. Sections 1.10.32 and
-              1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-              reproduced in their exact original form, accompanied by English
-              versions from the 1914 translation by H. Rackham.
+              My App is called Dota2 Heroes It's an app that shows the all the
+              Dota 2 heroes and if you click on hero gives you the detail and
+              attributes of that hero.
             </h5>
+            <div>
+              <a href="https://github.com/Degan90/Dota2-Heroes.git">
+                <img className="icon" src={greengh} />
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <a href="https://pedantic-edison-4080c1.netlify.app">
+              <img className="projectImage" src={heroes} />
+            </a>
           </div>
         </div>
-        <div>
-          <img className="projectImage" src={background} />
+
+        <div className="project">
+          <div>
+            <h2>Project 3</h2>
+            <div className="projectDes">
+              <h5>
+                he standard chunk of Lorem Ipsum used since the 1500s is
+                reproduced below for those interested. Sections 1.10.32 and
+                1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
+                reproduced in their exact original form, accompanied by English
+                versions from the 1914 translation by H. Rackham.
+              </h5>
+            </div>
+          </div>
+          <div>
+            <img className="projectImage" src={background} />
+          </div>
         </div>
-      </div>
-    </section>
+        <div className="project">
+          <div>
+            <h2>Project 4</h2>
+            <div className="projectDes">
+              <h5>
+                he standard chunk of Lorem Ipsum used since the 1500s is
+                reproduced below for those interested. Sections 1.10.32 and
+                1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
+                reproduced in their exact original form, accompanied by English
+                versions from the 1914 translation by H. Rackham.
+              </h5>
+            </div>
+          </div>
+          <div>
+            <img className="projectImage" src={background} />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
